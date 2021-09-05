517387a230b69a3f1c9abb8f481a1fb06b6669dd
alokhub <alokmts@gmail.com>
2021-09-05 15:25:09 +0530

    descending order added

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -17,7 +17,7 @@ int main()
     {
         for(int j=0;j<n-i-1;j++)
         {
-            if(arr[j]>arr[j+1])
+            if(arr[j]<arr[j+1])
             {
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
